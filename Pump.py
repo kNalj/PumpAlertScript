d451de44dcc17db5cfdecf336118a7195f8ff282
kNalj <drmishello69@gmail.com>
2020-03-17 12:53:36 +0100

    Add a dummy driver for pumps.

--- Pump.py
+++ Pump.py
@@ -1,10 +1,79 @@
 # This is a file that represents the pump instrument and all the communication with the intrument will be handeled from
 # this file
 
+from time import asctime
+from random import randint
+
 
 class Pump:
-    def __init__(self):
-        pass
+    """
+    TODO: Study how the instrument works and write the driver for it
+    """
+    def __init__(self, name, address, port):
+        """
+        TODO: Write documentation
+
+        :param name:
+        :param address:
+        :param port:
+        """
+        self.name = name
+        self.ip = address
+        self.port = port
+        self.last_check = "Never"
+        self.status = "Unknown"
+        self.temp = None
+        self.error = None
+
+        self.temp_limit = self.get_temp_limit()
+        self.alert_temp = self.temp_limit - (0.1 * self.temp_limit)
+
+    def check_status(self):
+        """
+        TODO: Write documentation
+
+        :return:
+        """
+        self.temp = self.get_temp()
+        self.error = self.get_errors()
+        self.last_check = asctime()
+        return self.temp, self.error
+
+    def get_temp(self):
+        """
+        TODO: Write documentation
+
+        :return:
+        """
+        # in reality i should get this from the instrument, this is just for testing
+        return randint(0, 100)
+
+    def get_errors(self):
+        """
+        TODO: Write documentation
+
+        :return:
+        """
+        # in reality i should get this from the instrument, this is just for testing
+        return False
+
+    def get_temp_limit(self):
+        """
+        TODO: Write documentation
+
+        :return:
+        """
+        # in reality i should get this from the instrument, this is just for testing
+        return 90
+
+    def set_temp_alert_limit(self, value):
+        """
+        TODO: Write documentation
+
+        :param value:
+        :return:
+        """
+        self.alert_temp = value
 
 
 def main():
